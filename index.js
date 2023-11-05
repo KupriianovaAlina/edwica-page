04eb0038b0ddceb26f22a67f3aee57a3e17a89cf
alina <akupriianova99@gmail.com>
2023-11-05 23:12:45 +0300

    add js

---
+++ index.js
@@ -0,0 +1,33 @@
+const features = document.querySelector('.main__features')
+const lists = features.querySelectorAll('.list');
+const navs = document.querySelectorAll('.nav__item');
+
+let currentActiveFeature = lists[0];
+let currentActiveNav = navs[0];
+
+lists.forEach((list) => {
+  const button = list.querySelector('.list__panel-bth');
+
+  button.addEventListener('click', (event) => {
+    let eventTargetList = event.target.closest('.list');
+    if (currentActiveFeature !== eventTargetList) {
+      eventTargetList.classList.remove('list_type_text-hidden');
+      currentActiveFeature.classList.add('list_type_text-hidden');
+      currentActiveFeature = list;
+    } else {
+      eventTargetList.classList.toggle('list_type_text-hidden');
+    }
+  });
+});
+
+navs.forEach((nav) => {
+  nav.addEventListener('click', (event) => {
+    const eventTargetNav = event.target.closest('.nav__item');
+
+    if (eventTargetNav !== currentActiveNav) {
+      currentActiveNav.classList.remove('nav__item_active');
+      eventTargetNav.classList.add('nav__item_active');
+      currentActiveNav = eventTargetNav;
+    }
+  })
+});
